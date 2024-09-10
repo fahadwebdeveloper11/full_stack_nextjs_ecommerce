c2a1d8755500cd255c838780e27f3fe05794115d
fahadwebdeveloper11 <fahadkhanweb202325@gmail.com>
2024-09-10 11:59:44 +0500

    more pages added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,17 @@
 const page = () => {
-  return <div>About</div>;
+  return (
+    <div className="flex flex-col p-10 gap-5 items-center justify-center w-full min-h-52 text-center">
+      <h1 className="font-medium text-2xl">This simple About Page </h1>
+      <p className="w-[90%] md:w-3/4">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere totam,
+        impedit sed pariatur nisi assumenda, officiis aliquam neque nam porro
+        quasi rerum possimus! Sapiente deleniti amet, doloribus voluptatem eaque
+        velit ipsam fugiat explicabo? Natus dolorum accusamus modi perferendis,
+        voluptatibus adipisci sequi. Placeat, natus? Amet, nesciunt. Ut sunt
+        consequuntur eveniet iure!
+      </p>
+    </div>
+  );
 };
 
 export default page;
